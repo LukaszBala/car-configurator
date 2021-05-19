abdb2726ec190778f89aaadfbc1e22f57041370d
pablito_chmiel <pawcio27610@wp.pl>
2021-05-19 21:55:33 +0200

    add first component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import logo from "./logo.svg";
 import "./App.scss";
+import SelectModel from "./components/selectModel";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <button>Importuj konfigurację</button>
+                <button>Utwórz nową konfigurację</button>
+                <SelectModel />
+            </header>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/selectModel.js
@@ -0,0 +1,14 @@
+import logo from "../logo.svg";
+
+const SelectModel = () => {
+    return (
+        <div>
+            <img src={logo} className="App-logo" alt="logo"/>
+            <img src={logo} className="App-logo" alt="logo"/>
+            <img src={logo} className="App-logo" alt="logo"/>
+            <a>fureiwufheiruhf</a>
+        </div>
+    )
+}
+
+export default SelectModel
