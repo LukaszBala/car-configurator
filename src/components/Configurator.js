2c77f89c8146fed054243b68947bc37f7d7096d4
pablito_chmiel <pawcio27610@wp.pl>
2021-05-22 12:51:58 +0200

    add more check box group

--- src/components/Configurator.js
+++ src/components/Configurator.js
@@ -5,7 +5,11 @@ const Configurator = () => {
     return (
         <div className="App">
             <Picture />
+            <CheckBoxGroup name={"model"} values={["3","5","7"]}/>
+            <CheckBoxGroup name={"nadwozie"} values={["sedan","tfu kombi","kupe kabrio"]}/>
             <CheckBoxGroup name={"silnik"} values={["diesel","benzyna","jebane V8"]}/>
+            <CheckBoxGroup name={"wyposażenie"} values={["bieda","ujdzie","lukus","somsiad placze jak widzi"]}/>
+            <CheckBoxGroup name={"felgi"} values={["18","19","32 z ursusa"]}/>
         </div>
     )
 }
