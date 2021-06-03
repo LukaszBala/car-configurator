19fec569247bf7dcbe13121c3e642aacecd3fb15
Łukasz Bala <lukasz.bala@msleopard.com>
2021-06-03 17:21:22 +0200

    [option-template][ŁuBa] update

---
+++ src/components/Option/Option.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Option.scss';
+import CheckBoxGroup from "../CheckBox/CheckBoxGroup";
+
+const Option = ({name, values}) => (
+    <div className="Option">
+        <h1> {name} </h1>
+        <div className="content">
+            <CheckBoxGroup name={name} values={values}/>
+            <div className="svg">
+                <svg width="100" height="100">
+                    <circle cx="50" cy="50" r="40" stroke="green" stroke-width="4" fill="yellow" />
+                </svg>
+            </div>
+        </div>
+    </div>
+);
+
+Option.propTypes = {};
+
+Option.defaultProps = {};
+
+export default Option;
