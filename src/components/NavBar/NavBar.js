0adf993848b683bf5e3284805ad7370cdd19ed2b
Łukasz Bala <lukasz.bala@msleopard.com>
2021-06-07 17:38:34 +0200

    [ŁuBa] update

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import './NavBar.scss';
 
 const NavBar = () => (
     <div className="NavBar">
-        <a className="active" href="#home">Home</a>
-        <a href="#news">News</a>
-        <a href="#contact">Contact</a>
-        <a href="#about">About</a>
+        <a className="active" href="/">Home</a>
+        <a href="/config">Config</a>
     </div>
 );
 
