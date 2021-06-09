17eed445a2811d7e8bebe162f15f904e02c32347
Łukasz Bala <lukasz.bala@msleopard.com>
2021-06-09 18:11:52 +0200

    [ŁuBa] firebase

--- src/App.js
+++ src/App.js
@@ -3,21 +3,11 @@ import Configurator from "./components/Configurator";
 import Home from "./components/Home";
 import NavBar from "./components/NavBar/NavBar";
 import React from "react";
-import firebase from "firebase";
 
 function App() {
-    const firebaseApp = firebase.apps[0];
-    const starCountRef = firebase.database().ref();
-    let data;
-    starCountRef.on('value', (snapshot) => {
-        data = snapshot.val();
-    });
     return (
         <div>
-            {/*<NavBar/>*/}
-            <code>
-                <pre>{JSON.stringify(data, null, 2)}</pre>
-            </code>
+            <NavBar/>
             <BrowserRouter>
                 <Switch>
                     <Route exact path="/" component={Home} />
--- src/components/Configurator.js
+++ src/components/Configurator.js
@@ -1,6 +1,6 @@
-import CheckBoxGroup from "./CheckBox/CheckBoxGroup"
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import Option from './Option/Option'
+import firebase from "firebase";
 
 const Configurator = () => {
     const [car, setCar] = useState(
@@ -17,9 +17,19 @@ const Configurator = () => {
                 rims: ''
             }
         })
+
+    useEffect(() => {
+        const starCountRef = firebase.database().ref('cars/1');
+        starCountRef.once('value', (snapshot) => {
+        }).then(r => setCar(r.val()));
+
+    })
     return (
         <div className="App">
             <Option name={'model'} values={["1", "2", "3"]}/>
+            <code>
+                <pre>{JSON.stringify(car, null, 2)}</pre>
+            </code>
             <Option name={"nadwozie"} values={["sedan","tfu kombi","kupe kabrio"]}/>
             <Option name={"silnik"} values={["diesel","benzyna","jebane V8"]}/>
             <Option name={"wyposażenie"} values={["bieda","ujdzie","lukus","somsiad placze jak widzi"]}/>
