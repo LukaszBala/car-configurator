1a7a1986a6b5ec4a1335a6a79911e9236ce97716
Łukasz Bala <lukasz.bala@msleopard.com>
2021-06-09 17:01:17 +0200

    [ŁuBa] firebase

--- src/index.js
+++ src/index.js
@@ -3,6 +3,18 @@ import ReactDOM from "react-dom";
 import "./index.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import firebase from "firebase";
+
+firebase.initializeApp({
+    apiKey: "AIzaSyD5FQKuGQzxlt1Hj8DLMnlkJ8TDQLz92H0",
+    authDomain: "car-configurator-6b257.firebaseapp.com",
+    projectId: "car-configurator-6b257",
+    storageBucket: "car-configurator-6b257.appspot.com",
+    messagingSenderId: "1034811342781",
+    appId: "1:1034811342781:web:d15b16d0d5d3545a139cd4",
+    measurementId: "G-ELL08L4H93",
+    databaseURL: "https://car-configurator-6b257-default-rtdb.europe-west1.firebasedatabase.app/"
+});
 
 ReactDOM.render(
   <React.StrictMode>
