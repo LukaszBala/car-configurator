ec799eeff42831052ae7d3a96becdb76c5c7d6c0
pablito_chmiel <pawcio27610@wp.pl>
2021-06-11 15:56:55 +0200

    get all cars

--- src/components/Comparator/Comparator.js
+++ src/components/Comparator/Comparator.js
@@ -1,6 +1,23 @@
+import {useEffect, useState} from "react";
+
 const Comparator = () => {
+    const [cars, setCars] = useState(null);
+
+    useEffect(() => {
+        // GET request using fetch inside useEffect React hook
+        fetch("https://car-configurator-6b257-default-rtdb.europe-west1.firebasedatabase.app/cars.json")
+            .then(response => response.json())
+            .then(data => setCars(data));
+        console.log('get all');
+
+        // empty dependency array means this effect will only run once (like componentDidMount in classes)
+    }, []);
+
     return (
-        <div>
+        <div className="App">
+            <code>
+                <pre>{JSON.stringify(cars, null, 2)}</pre>
+            </code>
         </div>
     )
 }
