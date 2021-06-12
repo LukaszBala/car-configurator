7a131dcbe48f52ae6b4b7824808a0cd8926ff589
pablito_chmiel <pawcio27610@wp.pl>
2021-06-12 18:52:14 +0200

    i'm dumbass

--- src/components/Comparator/Comparator.js
+++ src/components/Comparator/Comparator.js
@@ -2,25 +2,23 @@ import {useEffect, useState} from "react";
 
 const Comparator = () => {
     const [cars, setCars] = useState({});
-    const [tab, setTab]=useState([]);
+    const [tab, setTab] = useState([]);
 
     useEffect(() => {
         // GET request using fetch inside useEffect React hook
         fetch("https://car-configurator-6b257-default-rtdb.europe-west1.firebasedatabase.app/cars.json")
             .then(response => response.json())
-            .then(data => setCars(data));
-        console.log('get all');
-        console.log(Object.keys(cars));
-        let t=Object.keys(cars);
-        console.log(cars[t[0]]);
-        console.log(cars["1"]);
-        let tempTab=[];
-        for( let i =0;i < t.length;i++ )
-            tempTab.push(cars[t[i]])
-        //cars.forEach(elem=>t.push(JSON.stringify(elem, null, 2)));
-        //t.push(cars.1.toString());
-
-        setTab(tempTab);
+            .then(data => setCars(data))
+            .then(() => {
+                console.log('get all');
+                console.log(Object.keys(cars));
+                let t = Object.keys(cars);
+                console.log(cars[t[0]]);
+                console.log(cars["1"]);
+                let tempTab = [];
+                t.forEach(elem => tempTab.push(cars[elem]));
+                setTab(tempTab)
+            });
 
         // empty dependency array means this effect will only run once (like componentDidMount in classes)
     }, []);
@@ -33,7 +31,7 @@ const Comparator = () => {
             <table>
                 <thead>
                 <tr>
-                    <th></th>
+                    <th>{""}</th>
                     {
                         tab.map((elem, index) => {
                             return (<th key={index}>{JSON.stringify(elem, null, 2)}</th>)
