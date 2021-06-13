82cb572651ad617c7e4b00cd9d00a316dfb4eac1
Łukasz Bala <lukasz.bala@msleopard.com>
2021-06-13 20:10:23 +0200

    [ŁuBa] update

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import './NavBar.scss';
 
-const NavBar = () => (
+const NavBar = () => {
+
+    function active(current) {
+        let url = window.location.href.split('/');
+        url = url[url.length - 1];
+        return url === current ? 'active' : null;
+    }
+    return (
     <div className="NavBar">
-        <a className="active" href="/">Home</a>
-        <a href="/config">Config</a>
-        <a href="/compare">Compare</a>
+        <a className={active('')} href="/">Home</a>
+        <a className={active('config')} href="/config">Config</a>
+        <a className={active('compare')} href="/compare">Compare</a>
+        {}
     </div>
-);
+)};
 
 NavBar.propTypes = {};
 
